fb6955b6fb5e8db3b2630aa3984d564bba46d5e4
Betsy Haibel <betsy.haibel@gmail.com>
2017-05-19 16:36:58 -0400

    add appropriate async behavior

--- src/store/ethograms.js
+++ src/store/ethograms.js
@@ -9,14 +9,24 @@ export default {
   },
   mutations: {
     setData (state, { data }) {
+      state.loading = false
       state.data = keyBy(data, 'id')
+    },
+    setLoading (state) {
+      state.loading = true
+    },
+    setError (state, { error }) {
+      state.loading = false
+      state.error = error
     }
   },
   actions: {
     fetchAll ({ commit }) {
+      commit('setLoading')
       fetch('/static/fixtures.json')
         .then(response => response.json())
         .then(response => commit('setData', { data: response.data }))
+        .catch(error => commit('setError', { error }))
     }
   }
 }
