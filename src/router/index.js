0e98c68fbdb571428ae0390e7d88004163a40b18
Betsy Haibel <betsy.haibel@gmail.com>
2017-05-19 13:35:18 -0400

    High-level route structure (#2)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -8,8 +7,41 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home'
+    },
+    {
+      path: '/login',
+      name: 'Login'
+    }
+    {
+      // list available ethograms
+      path: '/observation_sessions/start',
+      name: 'ObservationSessions.Start'
+    },
+    {
+      // subject names buttons
+      path: '/observations/:id/subject',
+      name: 'Observations.Subject'
+    },
+    {
+      // behavior names buttons
+      path: '/observations/:id/behavior',
+      name: 'Observations.Behavior'
+    },
+    {
+      // modifier names buttons
+      path: '/observations/:id/modifier',
+      name: 'Observations.Modifier'
+    },
+    {
+      // target names buttons
+      path: '/observations/:id/target',
+      name: 'Observations.Target'
+    },
+    {
+      // observation review page
+      path: '/observations/:id/review',
+      name: 'Observations.Review'
     }
   ]
 })
