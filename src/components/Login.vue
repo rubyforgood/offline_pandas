37dd1cd418dfd0082e6b316a8f0b37d1662da521
Julie Kwok <jkwok@xogrp.com>
2017-05-19 18:58:55 -0400

    Styles Login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,14 +3,14 @@
     <img src="../assets/renee-the-panda.png">
     <h1>Welcome!</h1>
     <h2> Please login</h2>
-    <div>
-      <input type="text" name="email" placeholder="Email" v-model="credentials.email" />
+    <div class="pv2">
+      <input class="w5 pa2" type="text" name="email" placeholder="Email" v-model="credentials.email" />
     </div>
-    <div>
-      <input type="password" name="password" placeholder="Password" v-model="credentials.password" />
+    <div class="pv2">
+      <input class="w5 pa2" type="password" name="password" placeholder="Password" v-model="credentials.password" />
     </div>
-    <div>
-      <input type="submit" class="btn bg-green b--green white ba br1" value="Log In" v-on:click="onSubmit" />
+    <div class="pv2">
+      <input type="submit" class="bg-green b--green white ba pv2 ph3" value="Log In" v-on:click="onSubmit" />
     </div>
   </div>
 </template>
@@ -47,8 +47,5 @@ export default {
 <style>
   .login {
     padding-top: 60px;
-  }
-  .btn {
-    padding: 10px 15px;
   }
 </style>
